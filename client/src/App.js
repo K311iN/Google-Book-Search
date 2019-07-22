9a978a61da78ec060f92f8d65488ebcd3bdb9c96
Kellin <kellinjoshua@gmail.com>
2019-07-21 23:34:08 -0700

    components

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Search from "./pages/Search";
+import Saved from "./pages/Saved";
+import Error from "./pages/Error"
+import Navbar from "./components/Navbar/navbar";
+import Main from "./components/Main/main";
+import Footer from "./components/Footer/footer";
+import "./app.css";
 
-function App() {
+//import books from "";
+
+function App() { 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="wrapper">
+        <Navbar />
+        <Main>
+          <Switch>
+            <Route exact path="/" component={Search} />
+            <Route exact path="/search" component={Search} />
+            <Route exact path="/saved" component={Saved} />
+            <Route component={Error} />
+          </Switch>
+        </Main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
